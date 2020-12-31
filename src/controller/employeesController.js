5c23fa3e41f0a9c41e8894d9afa003ee660be10a
Federico Berbara <feedee96@gmail.com>
2020-12-31 19:30:54 -0300

    Upload first methods working

--- src/controller/employeesController.js
+++ src/controller/employeesController.js
@@ -1,19 +1,56 @@
+//* Require the functions that interact with the database
 const employeeServices = require('../services/employeeService');
+const Employees = require('../models/employeesModel');
 
-exports.getEmployees = async () => {
+exports.getEmployees = async (req, res) => {
 
+    try {
+        const employees = await employeeServices.getEmployees();
+        res.json(employees);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 };
 
-exports.getEmployeesById = async () => {
+exports.getEmployeesById = async (req, res) => {
+    
+    let employee; 
 
+    try {
+        employee = await employeeServices.getOneEmployee(req.params.id);
+        res.json(employee);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
 };
 
 exports.getEmployeesByRole = async () => {
 
 };
 
-exports.postEmployee = async () => {
+exports.postEmployee = async (req, res) => {
 
+    const employeeData = new Employees({
+        name: req.body.name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        age: req.body.age
+    })
+
+    try {
+        const newEmployee = await employeeData.save();
+        res.status(201).json({
+            newEmployee
+        });
+    } catch (error) {
+        res.status(401).json({
+            message: error.message
+        });  
+    }
 };
 
 exports.updateEmployee = async () => {
--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,15 +1,15 @@
 const Employees = require('../models/employeesModel');
 
 const getEmployees = async () => {
-
+    return await Employees.find();
 };
 
-const getOneEmployee = async () => {
-
+const getOneEmployee = async (id) => {
+    return await Employees.findById(id);
 };
 
-const getEmployeesByRole = async () => {
-
+const getEmployeesByRole = async (req) => {
+    return await Employees.find(req);
 };
 
 const createEmployee = async () => {
