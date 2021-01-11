3d62a06ff7defb0cd37dc384531462d423236813
Federico Berbara <feedee96@gmail.com>
2021-01-11 16:54:21 -0300

    Endpoints modified

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -7,11 +7,10 @@ const employeesController = require('../controller/employeesController');
 
 //* Defined the methods
 router.get( '/', employeesController.getEmployees); //localhost:5000/employees/
-router.get( '/id/:id', employeesController.getEmployeesById); //localhost:5000/employees/id/1
-router.get( '/role/:role', employeesController.getEmployeesByRole); //localhost:5000/employees/role/admin
-router.post( '/addEmployee', employeesController.postEmployee); //localhost:5000/employees/addEmployee
-router.put( '/editEmployee/:id', employeesController.updateEmployee); //localhost:5000/employees/editEmployee/1
-router.delete( '/deleteEmployee/:id', employeesController.deleteOneEmployee); //localhost:5000/employees/deleteEmployee/1
-router.delete( '/deleteEmployees', employeesController.deleteAllEmployees); //localhost:5000/employees/deleteEmployees
+router.get( '/id/:id', employeesController.getEmployeesById); //localhost:5000/employees/id/xxxx
+router.get( '/role/:role', employeesController.getEmployeesByRole); //localhost:5000/employees/role/
+router.post( '/add', employeesController.postEmployee); //localhost:5000/employees/add
+router.put( '/update/:id', employeesController.updateEmployee); //localhost:5000/employees/update/xxxx
+router.delete( '/delete/:id', employeesController.deleteOneEmployee); //localhost:5000/employees/delete/xxxx
 
 module.exports = router;
